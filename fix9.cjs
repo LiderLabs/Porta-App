0f6fc389b2dcd1d4d532e4bb185b5d87b3895b8f
Jordan Owusu-Ansah <jordanowusu03@gmail.com>
2026-06-08 07:25:00 +0000

    feat: sidebar footer dots, profile name from convex, checkin overview, schedule badges

--- fix9.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs = require("fs");
-const path = "C:/Users/user/Desktop/porta-app/src/features/reception/pages/SchedulingPage.tsx";
-let lines = fs.readFileSync(path, "utf8").split("\n");
-const i = lines.findIndex(l => l.includes("printBadge"));
-if (i !== -1) { lines.splice(i, 1); console.log("removed printBadge at line", i+1); }
-fs.writeFileSync(path, lines.join("\n"), "utf8");
